bc0cc194d7e61a09784afea9c637291a09c4375c
xTylerCodes <tylerlafayette@gmail.com>
2017-04-12 22:58:08 -0500

    Added clock to main menu

--- src/renderer/components/content/HomeMenu.jsx
+++ src/renderer/components/content/HomeMenu.jsx
@@ -7,9 +7,62 @@ import {
 } from 'react-router-dom'
 
 export default class HomeMenu extends Component {
+  constructor() {
+    super()
+    this.state = {
+      time: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        diem: 'AM'
+      }
+    }
+  }
+  getTime() {
+    var currentTime = new Date();
+    var diem = 'AM';
+    var h = currentTime.getHours();
+    var m = currentTime.getMinutes();
+    var s = currentTime.getSeconds();
+
+    if (h === 0) {
+      h = 12;
+    } else if (h > 12) {
+      h = h - 12;
+      diem = 'PM';
+    }
+
+    if (m < 10) {
+      m = '0' + m;
+    }
+    if (s < 10) {
+      s = '0' + s;
+    }
+    var output = {
+      hours: h,
+      minutes: m,
+      seconds: s,
+      diem: diem
+    };
+    return output;
+  }
+  fetchTime() {
+    this.setState({
+      time: this.getTime()
+    })
+  }
+  componentWillMount() {
+    window.timeInterval = setInterval(this.fetchTime.bind(this), 100);
+  }
+  componentWillUnmount() {
+    clearInterval(window.timeInterval);
+  }
   render() {
     return (
       <div className='menu-wrapper'>
+        <div className='menu-top-left-wrapper'>
+          <span className='menu-time'>{this.state.time.hours}:{this.state.time.minutes} {this.state.time.diem}</span>
+        </div>
         <div className='menu-background'></div>
         <div className="app-icons-wrapper">
           <ReactCSSTransitionGroup
